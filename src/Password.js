93b2556435a9880f4db499d1db6329b31df7e7d9
CRSantiago <42612374+CRSantiago@users.noreply.github.com>
2022-06-15 14:31:42 -0400

    Feat: Allow generated passwords to be copied to clipboard

--- src/Password.js
+++ src/Password.js
@@ -1,8 +1,12 @@
 import React from "react"
 
 export default function Password(props){
+    function copyToClipboard(e) {
+        navigator.clipboard.writeText(e.target.firstChild.data)
+        alert("Copied password!")
+    }
     return (
-        <div className='password'>
+        <div className='password' onClick={copyToClipboard}>
             {props.password}
         </div>
     )
